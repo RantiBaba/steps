c435dd4f9bea4a7351abd9cc15308ec44d4dbfd0
RantiBaba <Olurantisarumi@gmail.com>
2023-09-08 19:44:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1);
+  };
+
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1);
+    if (step === 3) setStep(1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='steps' data-testid='steps'>
+      <div className='numbers' data-testid='numbers'>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step === 3 ? 'active' : ''}>3</div>
+      </div>
+
+      <p className='message' data-testid='message'>
+        <span data-testid='step'>Step {step}:</span>
+        <span data-testid='message-step'>{messages[step - 1]}</span>
+      </p>
+
+      <div className='buttons'>
+        <button
+          className='button'
+          onClick={handlePrevious}
+          data-testid='previous'
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <button className='button' onClick={handleNext} data-testid='next'>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
